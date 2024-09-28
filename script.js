e648d6980423e74adf08c030d81ce69ab884c887
ShubhamG07 <gs200397@gmail.com>
2024-09-28 23:18:02 +0530

    added more features to fetch weather forecast including current location feature

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const apiKey = '8R87UFWY68A5AU7KZSD3KPBDY';
 const currenContainer = document.getElementById("currentContainer");
 const currentDetails = document.getElementById('currentdetails');
 const currentImage = document.getElementById('currentimage');
+const forecastContainer =document.getElementById("forecast");
+
 
 document.getElementById('searchButton').addEventListener('click', () => {
     const location = document.getElementById('inputLocation').value;
@@ -10,7 +12,7 @@ document.getElementById('searchButton').addEventListener('click', () => {
 
 function fetchWeatherData(location) {
 
-    
+    forecastContainer.innerHTML = '';
 
     currentDetails.innerHTML = '';
     currentImage.innerHTML = '';
@@ -34,6 +36,67 @@ function fetchWeatherData(location) {
         });
 }
 
+
+// using current location button 
+
+document.getElementById('currentLocation').addEventListener('click', () => {
+    console.log("search button clicked");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            reverseGeocode(lat, lon);
+        }, error => {
+            console.error(error);
+            alert("Unable to retrieve your location. Please check your browser settings.");
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+});
+
+function reverseGeocode(lat, lon) {
+    const reverseGeocodeUrl = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`;
+
+    fetch(reverseGeocodeUrl)
+        .then(response => response.json())
+        .then(data => {
+            const locationName = data.display_name; 
+            fetchWeatherData(locationName);
+        })
+        .catch(error => {
+            console.error("Error fetching location name:", error);
+            alert("Could not retrieve location name.");
+        });
+}
+
+// function fetchWeather(lat, lon) {
+//     // Clear previous results
+//     forecastContainer.innerHTML = '';
+
+//     currentDetails.innerHTML = '';
+//     currentImage.innerHTML = '';
+
+//     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}?unitGroup=metric&key=${apiKey}&contentType=json`;
+
+//     fetch(url)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Location not found');
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             displayWeather(data);
+//         })
+//         .catch(error => {
+//             currenContainer.classList.remove("hide");
+//             currenContainer.classList.add("currentDetailsError");
+//             currentDetails.innerHTML = `<p style="font-weight:bold; font-size:25px">${error.message}</p>`;
+//         });
+// }
+
+
 function displayWeather(data) {
 
     currenContainer.classList.remove("currentDetailsError");
@@ -47,15 +110,17 @@ function displayWeather(data) {
 
 
 currenContainer.classList.remove("hide");
-    const weatherHTML = `
-        <h2> ${title}</h2>
-        <p>${temperature } </p>
-        <p>${condtion}</p>
-        <p>${humidity}</p>
-        <img src="icons/${data.currentConditions.icon}.png" alt="${data.currentConditions.conditions}" >
-    `;
+forecastContainer.classList.remove("hide");
+document.getElementById("forecastTitle").classList.remove("hide");
+    // const weatherHTML = `
+    //     <h2> ${title}</h2>
+    //     <p>${temperature } </p>
+    //     <p>${condtion}</p>
+    //     <p>${humidity}</p>
+    //     <img src="icons/${data.currentConditions.icon}.png" alt="${data.currentConditions.conditions}" >
+    // `;
   
-    // currentResult.innerHTML = weatherHTML;
+    // // currentResult.innerHTML = weatherHTML;
 
     let item1 = document.createElement("h2");
     item1.innerHTML=title;
@@ -79,14 +144,55 @@ currenContainer.classList.remove("hide");
 
     let item6 =document.createElement("p");
     item6.innerHTML=`${data.currentConditions.conditions}`;
-
+    
+//  let item8 = document.createElement("p");
+//  item8.innerHTML=`test : ${data.days[1].cloudcover}`
     
     currentDetails.appendChild(item1);
     currentDetails.appendChild(item2);
     currentDetails.appendChild(item3);
     currentDetails.appendChild(item4);
     currentDetails.appendChild(item7);
+    // currentDetails.appendChild(item8);
     currentImage.appendChild(item5);
     currentImage.appendChild(item6);
+
+    // loop function for 5 day forecast 
+
+    for(let i=1;i<6;i++){
+
+        // craeting each card for 1 day data 
+
+        let dataContainer =document.createElement("div");
+      
+
+        // creating and storing data of forecast for each day 
+
+        let data1 =document.createElement("p");
+        data1.innerHTML=`${data.days[i].datetime}`;
+        let data2 =document.createElement("p");
+        data2.innerHTML=`<img src="icons/${data.days[i].icon}.png" alt="${data.days[i].conditions}" >`;
+        let data3 =document.createElement("p");
+        data3.innerHTML=`Temp : ${data.days[i].temp} °C `;
+        let data4 =document.createElement("p");
+        data4.innerHTML=`Wind : ${data.days[i].windspeed} km/hr`;
+        let data5 =document.createElement("p");
+        data5.innerHTML=`Humidity : ${data.days[i].humidity} %`;
+
+        // appending all data to our container 
+
+        dataContainer.appendChild(data1);
+        dataContainer.appendChild(data2);
+        dataContainer.appendChild(data3);
+        dataContainer.appendChild(data4);
+        dataContainer.appendChild(data5);
+
+        dataContainer.classList.add("forecastCard");
+      
+        //appending it tour html div
+
+        forecastContainer.appendChild(dataContainer);
+
+    }
     
 }
